f9ddfa851342710db8e177f0cdaa5ab4607ccbab
Rafael Brendo <rafalive122@hotmail.com>
2024-02-29 14:45:00 -0300

    Dando efeito do rotate nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -25,7 +25,22 @@ for(let i=0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen"); //Toda vez que um card for aberto
+        //será criado uma classe chamada boxOpen
+        openCards.push(this);
+    }
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+
+}
+
+function checkMatch(){}
 
 
